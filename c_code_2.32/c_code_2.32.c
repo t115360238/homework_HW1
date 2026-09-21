5a13df8cd2d986fbca7dbeb6d697d5535dad5846
t115360238 <t115360238@ntut.org.tw>
2026-09-21 16:59:10 +0800

    add 2.32

--- c_code_2.32/c_code_2.32.c
+++ c_code_2.32/c_code_2.32.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 
 int main(){
-    
+    float weight, hight;
+
+    printf("enter weight(kg)\n");
+    scanf("%f", &weight);
+
+    printf("enter hight(m)\n");
+    scanf("%f", &hight);
+
+    printf("BMI: %.1f\n", weight / (hight * hight));
+    printf("Underweight: less than 18.5\n");
+    printf("Normal:      between 18.5 and 24.9\n");
+    printf("Overweight:  between 25 and 29.9\n");
+    printf("Obese:       30 or greater\n");
     return 0;
 }

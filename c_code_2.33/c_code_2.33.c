33803152a1b90979ea9b75894140d00bd83a288b
t115360238 <t115360238@ntut.org.tw>
2026-09-21 19:03:36 +0800

    add 2.33

--- c_code_2.33/c_code_2.33.c
+++ c_code_2.33/c_code_2.33.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 int main(){
+    int mileage, gas_money, km_driven, parking_fee, toll;
+
+    printf("Enter total mileage\n");
+    scanf("%d", &mileage);
+
+    printf("How much does a liter or gallon of gasoline cost\n");
+    scanf("%d", &gas_money);
     
+    printf("How many kilometers can it travel per liter or gallon on average\n");
+    scanf("%d", &km_driven);
+    
+    printf("Enter parking fee\n");
+    scanf("%d", &parking_fee);
+    
+    printf("Enter toll\n");
+    scanf("%d", &toll);
+
+    printf("Total cost:%d", ((mileage / km_driven) * gas_money) + parking_fee + toll);
+
     return 0;
 }

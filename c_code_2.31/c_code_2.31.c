400f5c6a6f22ef0fad52c69da8e40b220e75520b
t115360238 <t115360238@ntut.org.tw>
2026-09-21 16:44:19 +0800

    add 2.31

--- c_code_2.31/c_code_2.31.c
+++ c_code_2.31/c_code_2.31.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    
+
+    printf("number  square  cube\n");
+
+    for(int i = 1; i <= 10; i++){
+        printf("%d\t%d\t%d\n", i, i * i, i * i * i);
+    }
     return 0;
 }
